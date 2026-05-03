53a13ea1857dc37b973754831bebf739e193aae8
dhruvdarji207 <dhruvdarji207@gmail.com>
2026-05-03 13:00:29 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,10 @@
+a=10
+b=20
+c=a
+name="dhruv"
+sum=a+b
+print("sum of ",a'" and ",b," is ",sum)
+print("Type of a is ",type(a)," and id is ",id(a))
+print("Type of a is ",type(b)," and id is ",id(b))
+print("Type of a is ",type(c)," and id is ",id(c))
+print("Type of a is ",type(name)," and id is ",id(name))
